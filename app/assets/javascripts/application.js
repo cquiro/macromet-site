1f136fd53e64fc36743be48e605f47c934e39942
Carlos Quiroga <cquiro@gmail.com>
2015-10-30 11:28:57 -0500

    Add other pages and finish the main page frontend

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,28 +17,57 @@
 //= require_tree .
 
 $( document ).ready(function() {
+  // This slides a color square with the description of every team member
+  // over its picture when on hover.
   $('.team-member').hover(
-      function(){
-          $(this).find('.team-description').slideDown(250); //.fadeIn(250)
-      },
-      function(){
-          $(this).find('.team-description').slideUp(250); //.fadeOut(205)
-      }
+    function(){
+      $(this).find('.team-description').slideDown(250); //.fadeIn(250)
+    },
+    function(){
+      $(this).find('.team-description').slideUp(250); //.fadeOut(205)
+    }
   );
 
+  // This is to achieve animated scrolling.
   $(function() {
-  $('a[href*=#]:not([href=#myCarousel])').click(function () {
-  // $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
+    $('a[href*=#]:not([href=#myCarousel])').click(function () {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
       }
-    }
+    });
   });
-});
+
+  // This removes the active class from all li elements of a nav
+  // element and adds it to the element you click on.
+  $('.nav li a').click(function(e) {
+    $('.nav li').removeClass('active');
+    var $parent = $(this).parent();
+    if (!$parent.hasClass('active')) {
+        $parent.addClass('active');
+    }
+    e.preventDefault();
+  });
+
+
+  $("#exhibit").click(function() {
+    $("#porta-am, #porta-so").hide();
+    $("#porta-ex").show();
+  });
+
+  $("#ambient").click(function() {
+    $("#porta-ex, #porta-so").hide();
+    $("#porta-am").show();
+  });
+
+  $("#soluci").click(function() {
+    $("#porta-ex, #porta-am").hide();
+    $("#porta-so").show();
+  });
 });
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,33 +2,37 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
 
+  def setup
+    @base_title = "Macromet"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Macromet"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get portafolio" do
     get :portafolio
     assert_response :success
-    assert_select "title", "Portafolio | Macromet"
+    assert_select "title", "Portafolio | #{@base_title}"
   end
 
   test "should get proceso" do
     get :proceso
     assert_response :success
-    assert_select "title", "Proceso | Macromet"
+    assert_select "title", "Proceso | #{@base_title}"
   end
 
   test "should get historia" do
     get :historia
     assert_response :success
-    assert_select "title", "Historia | Macromet"
+    assert_select "title", "Historia | #{@base_title}"
   end
 
   test "should get contacto" do
     get :contacto
     assert_response :success
-    assert_select "title", "Contacto | Macromet"
+    assert_select "title", "Contacto | #{@base_title}"
   end
 end
