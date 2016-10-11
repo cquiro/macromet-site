1d83461a8f4d8f8e02a2a22b9206c99b4d21585b
Carlos Quiroga <cquiro@gmail.com>
2016-10-11 12:14:32 -0300

    Generate migration to add banner links, gitignore vim temp files, add link form field on banner edit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523232750) do
+ActiveRecord::Schema.define(version: 20161011145626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160523232750) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
+    t.string   "link"
   end
 
   create_table "messages", force: :cascade do |t|
