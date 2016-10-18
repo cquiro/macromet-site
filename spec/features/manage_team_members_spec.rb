35460c4b509943925766c13ab33c90f71a1d718b
Carlos Quiroga <cquiro@gmail.com>
2016-10-18 12:21:15 -0300

    Start model rspec tests

---
+++ spec/features/manage_team_members_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature "Manage Team Members" do
+  context "as a user" do
+    scenario "adds a new teammate"
+    scenario "deletes a teammate"
+    scenario "change attributes of a teammate"
+  end
+end
--- spec/models/teammate_spec.rb
+++ spec/models/teammate_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Teammate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a name, position and photo" do
+    teammate = Teammate.new(
+      name: "James Bond",
+      position: "Spy",
+      photo: "image")
+
+    expect(teammate).to be_valid
+  end
+  it "is invalid without a name"
+  it "is invalid without a position"
+  it "is invalid without an image"
 end
