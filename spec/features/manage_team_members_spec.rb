f15bb38d405a32b3238260a2864929c7f51932ec
Carlos Quiroga <cquiro@gmail.com>
2016-10-19 13:17:00 -0300

    Delete manage_temam_members_spec.rb

--- spec/features/manage_team_members_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-feature "Manage Team Members" do
-  context "as a user" do
-    scenario "adds a new teammate"
-    scenario "deletes a teammate"
-    scenario "change attributes of a teammate"
-  end
-end
