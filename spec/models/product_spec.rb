5c9c9f8812a2f104a74093a6ada1915ad596fe58
Carlos Quiroga <cquiro@gmail.com>
2016-11-08 16:49:53 -0300

    Include FactoryGirl in Product model tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,40 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   it "has a valid factory" do
-    expect(FactoryGirl.build(:product)).to be_valid
-  end
-
-  it "is valid with name, description, client, product_line and photo" do
-    product = Product.new(
-      name: "Grill",
-      description: "Charcoal kettle grill",
-      client: "Weber",
-      product_line: "Soluciones",
-      photo: "Some image")
-
-    expect(product).to be_valid
+    expect(build(:product)).to be_valid
   end
 
   it "is invalid without a name" do
-    product = Product.new(name: nil)
+    product = build(:product, name: nil)
     product.valid?
     expect(product.errors[:name]).to include("can't be blank")
   end
 
   it "is invalid without a description" do
-    product = Product.new(description: nil)
+    product = build(:product, description: nil)
     product.valid?
     expect(product.errors[:description]).to include("can't be blank")
   end
 
   it "is invalid without a client" do
-    product = Product.new(client: nil)
+    product = build(:product, client: nil)
     product.valid?
     expect(product.errors[:client]).to include("can't be blank")
   end
 
   it "is invalid without a product_line" do
-    product = Product.new(product_line: nil)
+    product = build(:product, product_line: nil)
     product.valid?
     expect(product.errors[:product_line]).to include("can't be blank")
   end
