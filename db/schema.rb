e12e72aec2b632f22c4814bfdac9cd74660c53c8
Carlos Quiroga <cquiro@gmail.com>
2016-11-08 10:22:30 -0300

    Add Type column to Products table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027235857) do
+ActiveRecord::Schema.define(version: 20161108132108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20161027235857) do
     t.string   "photo"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "type"
   end
 
   create_table "teammates", force: :cascade do |t|
