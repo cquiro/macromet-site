3432d44fb9e408b022aacbf128ba2d2c2c9b2f73
Carlos Quiroga <cquiro@gmail.com>
2016-11-15 15:56:03 -0300

    Clean assets folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,23 +9,23 @@ banner1 = Banner.create(name: "Banner 1",
             title: "IMPULSO E INNOVACIÓN PARA SU MARCA", 
             subtitle: "Ocho años conectando productos de calidad con el consumidor final",
             button: "Exhibición", 
-            image: "Some Image")
+            image: File.open(File.join(Rails.root, 'app/assets/images/Macromet_BannerExhibicion.jpg')))
 
 banner2 = Banner.create(name: "Banner 2", 
             title: "EXPANDIMOS NUESTROS HORIZONTES", 
             subtitle: "Diseño, producción y montaje de escenografía para Comedy Central Stand-Up Colombia",
             button: "Ambientación", 
-            image: "Some Image")
+            image: File.open(File.join(Rails.root, 'app/assets/images/Macromet_ComedyCentral4.jpg')))
 
 banner3 = Banner.create(name: "Banner 3", 
             title: "TRADICIÓN", 
             subtitle: "Más de 27 años construyendo país",
             button: "Nuestra Historia", 
-            image: "Some Image")
+            image: File.open(File.join(Rails.root, 'app/assets/images/Macromet_Familia_cortaNashville.jpg')))
 
 clients = Banner.create(name: "Clientes", 
             title: "Clientes Macromet", 
             subtitle: "Logos de los clientes de Macromet",
             button: "Clientes", 
-            image: "Some Image")
+            image: File.open(File.join(Rails.root, 'app/assets/images/clientes_macromet.jpg')))
 
