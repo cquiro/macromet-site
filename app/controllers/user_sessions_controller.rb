876f219c65b434d0046c19c64ab28a5678076718
Carlos Quiroga <cquiro@gmail.com>
2016-11-16 20:57:19 -0300

    Generate user_session_controller.rb and start testing

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,5 +3,10 @@ class UserSessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    user.authenticate(params[:password])
+    session[:user_id] = user.id 
+    flash[:success] = "Has iniciado sesión"
+    redirect_to contenido_path
   end
 end
--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -15,9 +15,40 @@ RSpec.describe UserSessionsController, type: :controller do
   end
 
   describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+    context "with correct credentials" do
+      before :each do
+        @user = create(:user)
+      end
+
+      it "redirects to contenido_path" do
+        post :create, email: "james@bond.com", password: "password"
+
+        expect(response).to be_redirect
+        expect(response).to redirect_to contenido_path
+      end
+
+      it "finds the user" do
+        expect(User).to receive(:find_by).with({
+          email: "james@bond.com" 
+        }).and_return(@user)
+        post :create, email: "james@bond.com", password: "password"
+      end
+
+      it "authenticates the user" do
+        allow(User).to receive(:find_by).and_return(@user)
+        expect(@user).to receive(:authenticate)
+        post :create, email: "james@bond.com", password: "password"
+      end
+
+      it "sets the user id in the session" do
+        post :create, email: "james@bond.com", password: "password"
+        expect(session[:user_id]).to eq @user.id
+      end
+
+      it "sets the flash success message" do
+        post :create, email: "james@bond.com", password: "password"
+        expect(flash[:success]).to eq "Has iniciado sesión"
+      end
     end
   end
 
